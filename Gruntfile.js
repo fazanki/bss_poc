7625bacb849ad7cfe60245bd01deb97862426dd0
Franco Zanki <zankian.vision@gmail.com>
2015-01-21 16:48:39 +0000

    enableing html5 mode for deep linking

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,25 +65,37 @@ module.exports = function (grunt) {
 
     // The actual grunt server settings
     connect: {
+
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35729,
+        // Modrewrite rule, connect.static(path) for each path in target's base
+        middleware: function (connect, options) {
+          var optBase = (typeof options.base === 'string') ? [options.base] : options.base,
+              middleware = [require('connect-modrewrite')(['!(\\..+)$ / [L]'])]
+                .concat(optBase.map(function (path) {
+                  if (path.indexOf('rewrite|') === -1) {
+                    return connect.static(path);
+                  } else {
+                    path = path.replace(/\\/g, '/').split('|');
+                    return  connect().use(path[1], connect.static(path[2]))
+                  }
+                }));
+
+          return middleware;
+        }
       },
       livereload: {
         options: {
           open: true,
-          middleware: function (connect) {
-            return [
-              connect.static('.tmp'),
-              connect().use(
-                '/bower_components',
-                connect.static('./bower_components')
-              ),
-              connect.static(appConfig.app)
-            ];
-          }
+          base: [
+            '.tmp',
+            'rewrite|/bower_components|./bower_components',
+            'rewrite|/app/styles|./app/styles', // for sourcemaps
+            '<%= yeoman.app %>'
+          ]
         }
       },
       test: {
