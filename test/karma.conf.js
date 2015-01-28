8c9ec24758c734d61e33c4386bad5fe0bd6c7cef
Franco Zanki <zankian.vision@gmail.com>
2015-01-28 10:38:07 +0000

    adding new components

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -41,6 +41,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-select/dist/select.min.js',
+      'bower_components/siftlogic.angular.treeview/angular.treeview.js'
 
       'app/*.js',
       'app/components/**/*.js',
